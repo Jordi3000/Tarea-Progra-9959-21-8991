5665bf2731f3f1d9afe71f33363d816422d236e5
Jordi3000 <jordanisrael150@gmail.com>
2023-02-27 21:05:44 -0600

    Laboratorio

--- Laboratorio.cpp
+++ Laboratorio.cpp
@@ -1,92 +1,98 @@
 /*En este apartado deberá entregar el laboratorio grupal  de vectores y matrices aplicado al caso "Universidad".  Este laboratorio deberá cumplir con los siguientes requisitos:
-
 1. Manejar tres(3) matrices ( Ingeniería, arquitectura y Administración)
-
 2. Las notas se generaran aleatoriamente y corresponderán a los exámenes: Primer Parcial (20 puntos), Segundo Parcial(25 puntos), Examen Final(35 puntos) y Actividades(20 puntos).  Se asume que las notas corresponden al promedio de todos los cursos asignados por alumno.  Se deberán calcular 10 alumnos por cada facultad.
-
 3. El programa deberá calcular quien es la mejor facultad, el mejor alumno por facultad, el mejor alumnos por las tres facultades y el promedio general de las tres.
-
 4. deberán desplegarse los datos detallados de cada facultad y los promedios solicitados.
-
 5. El proceso deberá llevarse a cabo a través de un ciclo WHILE dos veces.
-
 DEREK SAMAHEL THOMAS MONTERROSO 9959-21-7658 50%
 Jordan Josué Israel Rodríguez Ruano 9959-21-8991 50%
+LINK DEL VIDEO: https://drive.google.com/file/d/1DghZ1BlNozl009tjLy5kx-9VOo_1v3Du/view?usp=share_link
 */
-
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+
 using namespace std;
 
-const int NUM_FACULTADES = 3;
-const int NUM_ALUMNOS = 10;
-const int NUM_EVALUACIONES = 4;
-const string FACULTADES[NUM_FACULTADES] = {"Ingeniería", "Arquitectura", "Administración"};
+const int NUM_STUDENTS = 10; // Number of students per faculty
+const int NUM_FACULTIES = 3; // Number of faculties
 
-void generarNotas(float notas[][NUM_EVALUACIONES]);
-void calcularPromedios(float notas[][NUM_EVALUACIONES], float promedios[], float &promedioGeneral);
-int buscarMejorFacultad(float promedios[]);
-int buscarMejorAlumno(float notas[][NUM_EVALUACIONES], int numFacultad);
-int buscarMejorAlumnoTotal(float notas[][NUM_EVALUACIONES]);
-void imprimirDatos(float notas[][NUM_EVALUACIONES], float promedios[], float promedioGeneral);
+void generateGrades(int grades[][4]) {
+    srand(time(NULL));
+    for(int i=0; i<NUM_STUDENTS; i++) {
+        for(int j=0; j<4; j++) {
+            grades[i][j] = rand() % 101; // Random grades between 0 and 100
+        }
+    }
+}
+
+void displayGrades(int grades[][4], string faculty) {
+    cout << "Grades for " << faculty << " faculty: " << endl;
+    for(int i=0; i<NUM_STUDENTS; i++) {
+        cout << "Student " << i+1 << ": ";
+        for(int j=0; j<4; j++) {
+            cout << grades[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void calculateAverages(int grades[][4], double &average, double studentAverages[]) {
+    double totalSum = 0;
+    for(int i=0; i<NUM_STUDENTS; i++) {
+        double studentSum = 0;
+        for(int j=0; j<4; j++) {
+            studentSum += grades[i][j];
+        }
+        studentAverages[i] = studentSum / 4.0; // Average of student's grades
+        totalSum += studentAverages[i];
+    }
+    average = totalSum / NUM_STUDENTS; // Average of all students' averages
+}
+
+int getBestStudent(double studentAverages[]) {
+    int bestStudent = 0;
+    for(int i=1; i<NUM_STUDENTS; i++) {
+        if(studentAverages[i] > studentAverages[bestStudent]) {
+            bestStudent = i;
+        }
+    }
+    return bestStudent;
+}
 
 int main() {
-    srand(time(NULL));
-    float notas[NUM_FACULTADES][NUM_ALUMNOS][NUM_EVALUACIONES];
-    float promedios[NUM_FACULTADES];
-    float promedioGeneral;
+    int engineeringGrades[NUM_STUDENTS][4];
+    int architectureGrades[NUM_STUDENTS][4];
+    int administrationGrades[NUM_STUDENTS][4];
 
-    // Generar notas aleatorias
-    for (int i = 0; i < NUM_FACULTADES; i++) {
-        generarNotas(notas[i]);
-    }
+    // Generate grades for each faculty
+    generateGrades(engineeringGrades);
+    generateGrades(architectureGrades);
+    generateGrades(administrationGrades);
 
-    // Calcular promedios
-    calcularPromedios(notas[0], promedios, promedioGeneral);
+    // Display grades for each faculty
+    displayGrades(engineeringGrades, "Engineering");
+    displayGrades(architectureGrades, "Architecture");
+    displayGrades(administrationGrades, "Administration");
 
-    // Ciclo WHILE
-    int iteracion = 1;
-    while (iteracion <= 2) {
-        // Imprimir datos
-        imprimirDatos(notas[0], promedios, promedioGeneral);
+    // Calculate averages for each faculty
+    double engineeringAverage, architectureAverage, administrationAverage;
+    double engineeringStudentAverages[NUM_STUDENTS];
+    double architectureStudentAverages[NUM_STUDENTS];
+    double administrationStudentAverages[NUM_STUDENTS];
 
-        // Calcular promedios y buscar mejor facultad
-        calcularPromedios(notas[iteracion], promedios, promedioGeneral);
-        int mejorFacultad = buscarMejorFacultad(promedios);
+    calculateAverages(engineeringGrades, engineeringAverage, engineeringStudentAverages);
+    calculateAverages(architectureGrades, architectureAverage, architectureStudentAverages);
+    calculateAverages(administrationGrades, administrationAverage, administrationStudentAverages);
 
-        // Buscar mejor alumno por facultad y en total
-        int mejorAlumnoFacultad = buscarMejorAlumno(notas[iteracion], mejorFacultad);
-        int mejorAlumnoTotal = buscarMejorAlumnoTotal(notas[0]);
+    // Display averages for each faculty
+    cout << "Average grades for each faculty:" << endl;
+    cout << "Engineering: " << engineeringAverage << endl;
+    cout << "Architecture: " << architectureAverage << endl;
+    cout << "Administration: " << administrationAverage << endl;
 
-        // Imprimir resultados
-        cout << "\nMejor facultad: " << FACULTADES[mejorFacultad] << endl;
-        cout << "Mejor alumno de " << FACULTADES[mejorFacultad] << ": " << "Alumno " << mejorAlumnoFacultad + 1 << endl;
-        cout << "Mejor alumno en total: " << "Alumno " << mejorAlumnoTotal + 1 << endl;
-
-        iteracion++;
-    }
-
-    return 0;
-}
-
-// Función para generar notas aleatorias
-void generarNotas(float notas[][NUM_EVALUACIONES]) {
-    for (int i = 0; i < NUM_ALUMNOS; i++) {
-        for (int j = 0; j < NUM_EVALUACIONES; j++) {
-            notas[i][j] = (rand() % 51) / 10.0; // Generar número aleatorio entre 0 y 5 con una precisión de 1 decimal
-        }
-    }
-}
-
-// Función para calcular promedios
-void calcularPromedios(float notas[][NUM_EVALUACIONES], float promedios[], float &promedioGeneral)
-{
-    for (int i = 0; i < NUM_ALUMNOS; i++)
-    {
-        for (int j = 0; j < NUM_EVALUACIONES; j++)
-        {
-
-        }
-    }
+    // Calculate best student per faculty
+    int engineeringBestStudent = getBestStudent(engineeringStudentAverages);
+    int architectureBestStudent = getBestStudent(architectureStudentAverages);
+    int administrationBestStudent = getBestStudent(architectureStudentAverages);
 }
